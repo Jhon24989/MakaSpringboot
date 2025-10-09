dd125b88c741bdd345f88fd0be8303d1bb0bcb39
Jhon David Vanegas Gallo <jhon.vanegas@globant.com>
2025-10-09 17:44:59 -0500

    producto y product repository creados

--- src/main/java/repository/ProductoRepository.java
+++ src/main/java/repository/ProductoRepository.java
@@ -1,4 +1,6 @@
 package repository;
 
-public class ProductoRepository {
-}
+import model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> { }
