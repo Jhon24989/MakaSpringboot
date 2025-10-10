b03ef2c87b4db7060212890159e7f0ce7e58572c
Jhon David Vanegas Gallo <jhon.vanegas@globant.com>
2025-10-10 16:50:07 -0500

    creacion del Test CompraServiceTest

--- src/main/java/com/cafemaka/system/model/Producto.java
+++ src/main/java/com/cafemaka/system/model/Producto.java
@@ -27,12 +27,29 @@ public class Producto {
 
     // getters y setters
     public Long getId() { return id; }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
     public double getPrecio() { return precio; }
-    public void setPrecio(double precio) { this.precio = precio; }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
     public int getStock() { return stock; }
-    public void setStock(int stock) { this.stock = stock; }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
