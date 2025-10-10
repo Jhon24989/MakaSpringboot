bbc4b4ba2fd1a4afe154fb7bc08c9ef919a56982
Jhon David Vanegas Gallo <jhon.vanegas@globant.com>
2025-10-10 11:09:48 -0500

    institucionalController modificado

--- src/main/java/com/cafemaka/system/controller/InstitucionalController.java
+++ src/main/java/com/cafemaka/system/controller/InstitucionalController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.cafemaka.system.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
